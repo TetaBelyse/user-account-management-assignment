21ea333ced31ae551e31411eb35f3c0bc125ebaf
Teta Belyse <tetabelyse2@gmail.com>
2023-05-18 12:36:02 +0200

    users model

---
+++ models/User.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const UserSchema = new mongoose.Schema(
+  {
+    fName: {
+      type: String,
+      required: [true, "Provide the firstname"],
+    },
+    lName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Provide email"],
+      unique: [true, "your email is already used"],
+      match: [
+        /^[a-zA-Z0-9_.+]*[a-zA-Z][a-zA-Z0-9_.+]*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        "Provide a Valid Email",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      lowercase: true,
+      default: "user",
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    nationality: {
+      type: String,
+      required: true,
+    },
+    maritalStatus: {
+      type: String,
+      required: true,
+      enum: ["SINGLE", "MARRIED", "DIVORCED", "WIDOWED"],
+    },
+    password: {
+      type: String,
+      required: [true, "Provide password"],
+      minlength: 8,
+      select: false,
+    },
+    emailVerificationToken: {
+      type: string,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationStatus: {
+      type: String,
+      required: true,
+      enum: ["UNVERIFIED", "PENDING VERIFICATION", "VERIFIED"],
+      default: "PENDING VERIFICATION",
+    },
+    identificationNumber: {
+      type: String,
+      default: "",
+    },
+    identificationDocument: {
+      type: String,
+      default: "",
+    },
+    verificationMessage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Encrypt password using Bycrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", UserSchema);
