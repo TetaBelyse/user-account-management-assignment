7b59dd008b70f17fa754f67b93da1062bfa7ba83
Teta Belyse <tetabelyse2@gmail.com>
2023-05-18 11:32:05 +0200

    express app init

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+
+//home route
+app.get("/", (req, res) => {
+  res.json({ message: "server is running" });
+});
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
