aa01cf9c3f24583e871be231be70deb5793cb354
TetaBelyse <tetabelyse2@gmail.com>
2023-05-19 07:40:47 +0200

    updated app colors

--- src/components/constants/index.ts
+++ src/components/constants/index.ts
@@ -1,13 +1,7 @@
 export const appColors = {
-  GREEN: "#1d4511",
-  LIGHT_BLACK: "#17161e",
-  DARK_BLACK: "#121117",
-  GRAY: "#f3f3f3",
-  TEXT_WHITE: "#a1a1a3",
-  RED: "#fc3839",
-  WHITE: "#fff",
-  TEXT_GRAY: "#676767",
-  ORANGE: "#ff6a00",
+  DARK_GREEN: "#1d4511",
+  WHITE: "#FFF",
+  BLACK: "#000",
 };
 
 export const app = {
