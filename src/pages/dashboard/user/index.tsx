cc75042b94145f2e2f4e796551c3b4d0d372b35e
TetaBelyse <tetabelyse2@gmail.com>
2023-05-19 07:40:53 +0200

    updating user profile image

--- src/pages/dashboard/user/index.tsx
+++ src/pages/dashboard/user/index.tsx
@@ -1,10 +1,72 @@
 import { Box, Card, CardContent, CardHeader, Grid } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../reducers";
 import { AccountCircle, ImageRounded } from "@mui/icons-material";
+import { useState, useRef } from "react";
+import FullPageLoader from "../../../components/full-page-loader";
+import axios from "axios";
+import { app } from "../../../components/constants";
+import {
+  errorHandler,
+  setHeaders,
+  toastMessage,
+} from "../../../components/helpers";
+import { TOAST_MESSAGE_TYPES } from "../../../interfaces";
+import { setUser } from "../../../actions/user";
 
 function User() {
+  const dispatch = useDispatch();
   const userReducer = useSelector((state: RootState) => state.user);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const imageRef = useRef<HTMLInputElement>(null);
+
+  const uploadImage = (image: any) => {
+    return new Promise((resolve, reject) => {
+      const formData = new FormData();
+      formData.append("file", image);
+      axios
+        .post(app.FILE_UPLOAD_URL as string, formData)
+        .then((res) => {
+          resolve({ fileName: res.data.fileName });
+        })
+        .catch((error) => {
+          reject({
+            message: "Failed to upload the document, please try again later",
+          });
+        });
+    });
+  };
+
+  const handleFileUpload = async (image: any) => {
+    setIsLoading(true);
+    uploadImage(image)
+      .then((res: any) => {
+        const uploadedFile = res.fileName;
+        axios
+          .put(
+            app.BACKEND_URL + "/users/profile/",
+            {
+              image: uploadedFile,
+            },
+            setHeaders(userReducer.token)
+          )
+          .then((res) => {
+            setIsLoading(false);
+            toastMessage(TOAST_MESSAGE_TYPES.SUCCESS, res.data.responseMessage);
+            dispatch(setUser({ ...userReducer, profilePhoto: uploadedFile }));
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            errorHandler(error);
+          });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toastMessage(TOAST_MESSAGE_TYPES.ERROR, error.message);
+      });
+  };
+
   return (
     <div>
       <Grid container spacing={2}>
@@ -64,16 +126,31 @@ function User() {
                   {userReducer.profilePhoto.trim() === "" ? (
                     <AccountCircle style={{ fontSize: 100 }} />
                   ) : (
-                    <></>
+                    <img
+                      src={app.FILE_URL + userReducer.profilePhoto}
+                      style={{ width: 100, height: 100, borderRadius: 100 }}
+                    />
                   )}
                   <br />
-                  <button className="btn common-btn">Update Photo</button>
+                  <input
+                    type="file"
+                    onChange={(t: any) => handleFileUpload(t.target.files[0])}
+                    className="d-none"
+                    ref={imageRef}
+                  />
+                  <button
+                    className="btn common-btn mt-2"
+                    onClick={() => imageRef.current && imageRef.current.click()}
+                  >
+                    Update Photo
+                  </button>
                 </Grid>
               </Grid>
             </CardContent>
           </Card>
         </Grid>
       </Grid>
+      <FullPageLoader open={isLoading} />
     </div>
   );
 }
