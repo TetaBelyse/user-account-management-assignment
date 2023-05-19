e1ca77665773b9e6243e8068ceae4767e49de986
TetaBelyse <tetabelyse2@gmail.com>
2023-05-19 07:40:48 +0200

    user login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,154 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import "../../scss/login.scss";
+import { app, appColors } from "../../components/constants";
+import { IUser, TOAST_MESSAGE_TYPES, USER_ROLE_ENUM } from "../../interfaces";
+import { errorHandler, toastMessage } from "../../components/helpers";
+import { setUser } from "../../actions/user";
+import { CircularProgress } from "@mui/material";
+import { Home } from "@mui/icons-material";
 
+const initialState = { emailOrPhone: "", password: "" };
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [state, setState] = useState(initialState);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    axios
+      .post(app.BACKEND_URL + "/users/login/", state)
+      .then((res) => {
+        setIsSubmitting(false);
+        const {
+          _id,
+          fName,
+          lName,
+          email,
+          role,
+          gender,
+          age,
+          dob,
+          nationality,
+          maritalStatus,
+          isEmailVerified,
+          verificationStatus,
+          identificationNumber,
+          identificationDocument,
+          verificationMessage,
+          profilePhoto,
+          token,
+        } = res.data;
+        dispatch(
+          setUser({
+            fName,
+            lName,
+            email,
+            role,
+            gender,
+            age,
+            dob,
+            nationality,
+            maritalStatus,
+            isEmailVerified,
+            verificationStatus,
+            identificationNumber,
+            identificationDocument,
+            verificationMessage,
+            profilePhoto,
+            token,
+          } as IUser)
+        );
+        toastMessage(TOAST_MESSAGE_TYPES.SUCCESS, "Logged in successful");
+        if (role === USER_ROLE_ENUM.ADMIN) {
+          navigate("/dashboard");
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        errorHandler(error);
+      });
+  };
+
+  return (
+    <div className="login-main-container">
+      <div className="login-background-container">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="form-main-container">
+        <Link to="/">
+          <img
+            src={require("../../assets/logo.png")}
+            alt="logo"
+            style={{ width: 50 }}
+          />
+        </Link>
+
+        <div className="form-container">
+          <h2>Welcome to KITMESSAGE</h2>
+          <p>Login to continue</p>
+          <form method="post" onSubmit={(e) => handleSubmit(e)}>
+            <div className="form-group">
+              <label>Email Or Phone</label>
+              <input
+                className="form-control"
+                placeholder="Enter your email or phone number"
+                disabled={isSubmitting}
+                type="text"
+                name="email"
+                value={state.emailOrPhone}
+                onChange={(e) =>
+                  setState({ ...state, emailOrPhone: e.target.value })
+                }
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+                value={state.password}
+                disabled={isSubmitting}
+                onChange={(e) =>
+                  setState({ ...state, password: e.target.value })
+                }
+                required
+              />
+            </div>
+            {isSubmitting ? (
+              <button type="button" disabled={true}>
+                <CircularProgress />
+                &nbsp; Logging in...
+              </button>
+            ) : (
+              <button type="submit">Login</button>
+            )}
+          </form>
+          <div className="forget-password-container">
+            <Link to="#">Forget password?</Link>
+          </div>
+          <hr />
+          <div className="form-footer2">
+            <span>New?</span>
+            <Link to="/register">CREATE ACCOUNT</Link>
+          </div>
+          <div className="text-center mt-2">
+            <Link to="/" style={{ color: appColors.DARK_GREEN }}>
+              <Home /> Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
