d168d059a104cfd0a0fa545ffc6dd2d66d6216c4
TetaBelyse <tetabelyse2@gmail.com>
2023-05-19 17:59:03 +0200

    multifactor login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,18 +4,50 @@ import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import "../../scss/login.scss";
 import { app, appColors } from "../../components/constants";
-import { IUser, TOAST_MESSAGE_TYPES, USER_ROLE_ENUM } from "../../interfaces";
+import { IUser, TOAST_MESSAGE_TYPES } from "../../interfaces";
 import { errorHandler, toastMessage } from "../../components/helpers";
 import { setUser } from "../../actions/user";
 import { Home } from "@mui/icons-material";
 import FullPageLoader from "../../components/full-page-loader";
 
-const initialState = { email: "", password: "" };
+const initialState = { email: "", password: "", otp: "" };
+enum LOGIN_STEPS_ENUM {
+  CREDENTIALS = "CREDENTIALS",
+  OTP = "OTP",
+}
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [state, setState] = useState(initialState);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [activeStep, setActiveStep] = useState(LOGIN_STEPS_ENUM.CREDENTIALS);
+  const handleSubmit0 = (e: any) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    axios
+      .post(app.BACKEND_URL + "/users/login/init/", state)
+      .then((res) => {
+        setIsSubmitting(false);
+        setActiveStep(LOGIN_STEPS_ENUM.OTP);
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        errorHandler(error);
+      });
+  };
+  const handleResendToken = () => {
+    setIsSubmitting(true);
+    axios
+      .post(app.BACKEND_URL + "/users/resendotp/", state)
+      .then((res) => {
+        setIsSubmitting(false);
+        toastMessage(TOAST_MESSAGE_TYPES.SUCCESS, res.data.responseMessage);
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        errorHandler(error);
+      });
+  };
   const handleSubmit = (e: any) => {
     e.preventDefault();
     setIsSubmitting(true);
@@ -91,45 +123,85 @@ function Login() {
         <div className="form-container">
           <h2>Welcome</h2>
           <p>Login to continue</p>
-          <form method="post" onSubmit={(e) => handleSubmit(e)}>
-            <div className="form-group">
-              <label>Email Or Phone</label>
-              <input
-                className="form-control"
-                placeholder="Enter your email or phone number"
-                disabled={isSubmitting}
-                type="text"
-                name="email"
-                value={state.email}
-                onChange={(e) => setState({ ...state, email: e.target.value })}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter your password"
-                value={state.password}
-                disabled={isSubmitting}
-                onChange={(e) =>
-                  setState({ ...state, password: e.target.value })
-                }
-                required
-              />
-            </div>
-            {isSubmitting ? (
-              <button type="button" disabled={true}>
-                Logging in...
+          {activeStep === LOGIN_STEPS_ENUM.CREDENTIALS && (
+            <>
+              <form method="post" onSubmit={(e) => handleSubmit0(e)}>
+                <div className="form-group">
+                  <label>Email Or Phone</label>
+                  <input
+                    className="form-control"
+                    placeholder="Enter your email"
+                    disabled={isSubmitting}
+                    type="text"
+                    name="email"
+                    value={state.email}
+                    onChange={(e) =>
+                      setState({ ...state, email: e.target.value })
+                    }
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter your password"
+                    value={state.password}
+                    disabled={isSubmitting}
+                    onChange={(e) =>
+                      setState({ ...state, password: e.target.value })
+                    }
+                    required
+                  />
+                </div>
+                {isSubmitting ? (
+                  <button type="button" disabled={true}>
+                    Logging in...
+                  </button>
+                ) : (
+                  <button type="submit">Login</button>
+                )}
+              </form>
+              <div className="forget-password-container">
+                <Link to="#">Forget password?</Link>
+              </div>
+            </>
+          )}
+          {activeStep === LOGIN_STEPS_ENUM.OTP && (
+            <form method="post" onSubmit={(e) => handleSubmit(e)}>
+              <div className="form-group">
+                <label>Please enter an OTP code sent to : {state.email}</label>
+                <input
+                  className="form-control"
+                  placeholder="Enter OTP Code"
+                  disabled={isSubmitting}
+                  type="number"
+                  name="otp"
+                  value={state.otp}
+                  onChange={(e) => setState({ ...state, otp: e.target.value })}
+                  required
+                />
+              </div>
+              {isSubmitting ? (
+                <button type="button" disabled={true}>
+                  Verifying...
+                </button>
+              ) : (
+                <button type="submit">Verify OTP</button>
+              )}
+              <button
+                type="button"
+                style={{
+                  background: "transparent",
+                  color: appColors.DARK_GREEN,
+                }}
+                onClick={() => handleResendToken()}
+              >
+                Resend OTP
               </button>
-            ) : (
-              <button type="submit">Login</button>
-            )}
-          </form>
-          <div className="forget-password-container">
-            <Link to="#">Forget password?</Link>
-          </div>
+            </form>
+          )}
           <hr />
           <div className="form-footer2">
             <span>New?</span>
